eed7ac754d698eb97c6556cf9f366533fcf29565
Juan Yepes <juanferhiguita65@gmail.com>
2024-10-19 19:14:52 -0500

    Add: Angular material simple example project

--- Angular-Material/src/app/app.component.ts
+++ Angular-Material/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+//Podemos importar el boton a usar o todo el modulo de botones
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
